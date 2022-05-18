24e59bd325bb236d7f273942226afd7b11023776
faisalantu <faisalantu@gmail.com>
2022-05-19 04:12:53 +0600

    😜 axios and firebase added

---
+++ src/axiosConfig.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://neko-todo-asp.herokuapp.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// instance.interceptors.request...
+// instance.interceptors.request.use(
+//   (request) => {
+//     // Edit request config
+//     const token = localStorage.getItem("accessToken");
+//     if (token) {
+//       request.headers.common["authHeader"] = token;
+//     }
+//     //console.log("[request send-]",request);
+//     return request;
+//   },
+//   (error) => {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
